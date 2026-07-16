d780002d0226ed864f5df1a21c6293303b920459
Farnam <taheri.farnam@gmail.com>
2026-07-16 19:09:07 +0330

    fix: simplify resilience bootstrap

--- src/huntx/core/__init__.py
+++ src/huntx/core/__init__.py
@@ -1,30 +1,7 @@
 """Core orchestration package."""
 
-from typing import Any
+from .runtime_resilience import apply_runtime_resilience
 
-from . import optimized_orchestrator as _optimized
-from . import runtime_resilience as _runtime
-
-
-_original_canonical_sources = _runtime._canonical_ingestion_sources
-
-
-async def _canonical_sources_with_compatible_connector(
-    self: Any,
-    sources: list[Any],
-) -> list[Any]:
-    """Honor the public connector symbol used by existing integrations/tests."""
-    previous = _runtime.WindowedTelegramUserConnector
-    _runtime.WindowedTelegramUserConnector = _optimized.WindowedTelegramUserConnector
-    try:
-        return await _original_canonical_sources(self, sources)
-    finally:
-        _runtime.WindowedTelegramUserConnector = previous
-
-
-_runtime._canonical_ingestion_sources = _canonical_sources_with_compatible_connector
-_runtime.apply_runtime_resilience()
-
-apply_runtime_resilience = _runtime.apply_runtime_resilience
+apply_runtime_resilience()
 
 __all__ = ["apply_runtime_resilience"]
